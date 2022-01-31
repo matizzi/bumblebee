3b3339d082ee0c48d3dd6f1b46fa7d9859d7dc09
afilipamp <afilipa.mp.99@gmail.com>
2022-01-31 00:24:19 +0000

    purchase form

---
+++ assets/js/purchaseForm.js
@@ -0,0 +1,7 @@
+const formDest=document.getElementById('formDest');
+const select=document.getElementById('depcity, packchoice');
+const date=document.getElementById('departure');
+
+function validateForm1(){
+
+}
