d7537537f03444ebcdb8b102c4f0d2dcf18f5049
afilipamp <afilipa.mp.99@gmail.com>
2022-01-31 16:52:08 +0000

    formulário de compras

--- assets/js/purchaseForm.js
+++ assets/js/purchaseForm.js
@@ -1,7 +1,17 @@
-const formDest=document.getElementById('formDest');
-const select=document.getElementById('depcity, packchoice');
-const date=document.getElementById('departure');
+// const formDest=document.getElementById('formDest');
+// const select=document.getElementById('depcity, packchoice');
+// const date=document.getElementById('departure');
 
-function validateForm1(){
 
+function validateFormDest(){
+    var depcity=document.formdest.depcity.value;
+    var packchoice=document.formdest.packchoice.value;
+
+    if (depcity == "") {
+        alert("Please choose an option");
+        return false;
+    }else if (packchoice == ""){
+        alert("Please choose an option");
+        return false;
+    }
 }
