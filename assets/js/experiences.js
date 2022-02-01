56f9fd19d5b57a20250db18778a1d5c9ff8d7308
afilipamp <afilipa.mp.99@gmail.com>
2022-02-01 21:53:07 +0000

    info dinâmica form compras

--- assets/js/experiences.js
+++ assets/js/experiences.js
@@ -56,6 +56,19 @@ async function addExperienceHome(){
     document.getElementById("mostWantedExperiences").innerHTML = generatedHtml;
 }
 
+// async function addExperienceSelectField(){
+//     let generatedHtml="";
+//     let url = "json/experiences.json";
+//     let jsonFile = await fetch(url);
+//     let experiences = await jsonFile.json();
+//     for (let experiencia of experiences){    
+//         generatedHtml += '<option value="' +experiencia.experienceCity.toLowerCase()+ '">' +experiencia.experienceName+ '</option>'
+//         }
+//     document.getElementById("packchoice").innerHTML = generatedHtml;
+// }
+
+
+
 async function addExperienceSelectField(){
     let generatedHtml="";
     let url = "json/experiences.json";
@@ -64,10 +77,17 @@ async function addExperienceSelectField(){
     for (let experiencia of experiences){    
         generatedHtml += '<option value="' +experiencia.experienceCity.toLowerCase()+ '">' +experiencia.experienceName+ '</option>'
         }
-    document.getElementById("addExperienceSelectOptions").innerHTML = generatedHtml;
+    document.getElementById("packchoice").innerHTML = generatedHtml;
 }
 
+
+
+
+
+
+
 async function addExperienceRowToTable(){
+
     let generatedHtml="";
     let url = "json/experiences.json";
     let jsonFile = await fetch(url);
@@ -77,9 +97,55 @@ async function addExperienceRowToTable(){
         generatedHtml += '<td>' +experiencia.experienceName+ '</td>'
         generatedHtml += '<td>' +experiencia.experienceCity+ '</td>'
         generatedHtml += '<td>' +experiencia.numberOfDays+ '</td>'
-        generatedHtml += '<td>' +experiencia.numberOfPassagenrs+ '</td>'
+        generatedHtml += '<td>' +experiencia.numberOfPassengers+ '</td>'
         generatedHtml += '<td>' +experiencia.experiencePrice+ '</td>'
         generatedHtml += '</tr>'
     }
     document.getElementById("addExperienceToTable").innerHTML = generatedHtml;
 }    
+
+
+
+
+// let sortDirection = false;
+
+
+// window.onload = () => {
+//     addExperienceRowToTable(experiences);
+// }
+
+// async function addExperienceRowToTable(){
+//     let generatedHtml="";
+//     let url = "json/experiences.json";
+//     let jsonFile = await fetch(url);
+//     let experiences = await jsonFile.json();
+//     for (let experiencia of experiences){
+//         generatedHtml += '<tr>'
+//         generatedHtml += '<td>' +experiencia.experienceName+ '</td>'
+//         generatedHtml += '<td>' +experiencia.experienceCity+ '</td>'
+//         generatedHtml += '<td>' +experiencia.numberOfDays+ '</td>'
+//         generatedHtml += '<td>' +experiencia.numberOfPassengers+ '</td>'
+//         generatedHtml += '<td>' +experiencia.experiencePrice+ '</td>'
+//         generatedHtml += '</tr>'
+//     }
+//     document.getElementById("addExperienceToTable").innerHTML = generatedHtml;
+// }    
+
+// function sortColumn(experienceName){
+//     const dataType = typeof experiences [0][experienceName];
+
+//     sortDirection = !sortDirection;
+
+//     switch(dataType) {
+//         case 'number':
+//             sortNumberColumn(sortDirection, experienceName);
+//             break;
+//     }
+//     addExperienceRowToTable(experiences);
+// }
+
+// function sortNumberColumn(sort, experienceName){
+//     experiences = experiences.sort((exp1, exp2) =>{
+//         return sort ? exp1[experienceName] - exp2[experienceName] : exp2[experienceName] - exp1[experienceName]
+//     });
+// }
